7ad876a27d551b93bd54347604def1ae36be5172
Igor Golm <igor.golm@devecto.com>
2021-11-07 17:06:24 +0200

    Added super_app repo

---
+++ repos/super_app/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+terraform {
+  backend "local" {
+    path = "./.terraform.tfstate"
+  }
+}
+
+module "codecommit" {
+source = "git@github.com:igorgolm/dev-demo-modules.git//codecommit?ref=v0.0.1"
+  
+  repository_name   = var.repository_name
+  description       = var.description
+  default_branch    = var.default_branch
+}
---
+++ repos/super_app/variables.tf
@@ -0,0 +1,11 @@
+variable "repository_name" {
+  type        = "super-app"
+}
+
+variable "description" {
+  default     = "Demo app repo"
+}
+
+variable "default_branch" {
+  default     = "master"
+}
